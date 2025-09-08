53716abe98ec613e299cd059a6c41c463b80dca8
MrErik402 <bujakierikattila@turr.hu>
2025-09-08 17:53:02 +0200

    Házifeladat megoldás

--- javascript/app.js
+++ javascript/app.js
@@ -1,56 +1,69 @@
-
 let nameField = document.getElementById("nameField");
 let priceField = document.getElementById("priceField");
 let countField = document.getElementById("countField");
 let addBtn = document.getElementById("addBtn");
 let itemsList = document.getElementById("itemsList");
 let sumLbl = document.getElementById("sumLbl");
-let productInList = document.getElementById("productInList");
+let nameFieldList = document.getElementById("termekek")
 
 let items = [];
-let productList = []; /* név, ár */
+let products = [];
 
 addBtn.addEventListener('click', () => {
-    if ((nameField.value == '' || productInList.value == '')|| priceField.value == 0 || countField.value == 0) {
+
+    if (nameField.value == '' || priceField.value == 0 || countField.value == 0) {
         window.alert("Nem adtál meg minden adatot!");
         return;
     }
 
-    if(nameField.value != ""){
-        items.push({
-            name: nameField.value,
-            price: Number(priceField.value),
-            count: Number(countField.value),
-            sum: priceField.value * countField.value
-        });
+    items.push({
+        name: nameField.value,
+        price: Number(priceField.value),
+        count: Number(countField.value),
+        sum: priceField.value * countField.value
+    });
 
-        for (let i = 0; i < items.length; i++) {
-            if(!productList.find(x => x.name == items[i].name)){
-                productList.push({
-                    name: items[i].name,
-                    price: items[i].price,
-                })
-            }
-            
+    let meglevoTermek = products.find(product => product.name == nameField.value)
+    if (meglevoTermek) {
+        if (meglevoTermek.price != Number(priceField.value)) {
+            items.forEach(item => {
+                if (item.name == nameField.value) {
+                    item.price = Number(priceField.value);
+                    item.sum = item.price * item.count;
+                }});
+            meglevoTermek.price = Number(priceField.value);
         }
-    }else{
-        items.push({
-            name: productInList.value,
-            price: Number(priceField.value),
-            count: Number(countField.value),
-            sum: priceField.value * countField.value
-        })
-    } 
+    }
+    else {
+        products.push({
+            name: nameField.value,
+            price: Number(priceField.value)
+        });
+    }
 
     RefreshTable();
     ClearForm();
     Save();
-    LoadProduct();
 });
 
-function RefreshTable(){
+nameField.addEventListener('selectionchange', () => {
+    let item = products.find(product => product.name == nameField.value);
+    if (items) {
+        priceField.value = item.price;
+    }
+});
+
+
+function RefreshTable() {
     itemsList.innerHTML = "";
+    nameFieldList.innerHTML = "";
     let sum = 0;
+
+    for (let j = 0; j < products.length; j++) {
+        let optionData = document.createElement('option')
+        optionData.value = products[j].name;
+        nameFieldList.appendChild(optionData)
+    }
 
     for (let i = 0; i < items.length; i++) {
 
@@ -61,24 +74,25 @@ function RefreshTable(){
         let td4 = document.createElement('td');
         let td5 = document.createElement('td');
         let td6 = document.createElement('td');
-        let btn = document.createElement('button');
 
-        td1.innerHTML = i+1;
+        let btn = document.createElement("button");
+
+        td1.innerHTML = i + 1;
         td2.innerHTML = items[i].name;
         td3.innerHTML = items[i].price + ' Ft';
         td4.innerHTML = items[i].count + ' db';
-        td5.innerHTML = items[i].sum + ' Ft'; 
-        btn.innerHTML = "X"
+        td5.innerHTML = items[i].sum + ' Ft';
+        btn.innerHTML = "X";
 
         td3.classList.add('text-end');
         td4.classList.add('text-end');
         td5.classList.add('text-end');
-        btn.classList.add('btn,btn-danger,text-center,align-middle');
-        btn.addEventListener('click', () => {
-            Delete(i);
-        })
-    
+        td6.classList.add('text-center');
+        btn.classList.add('btn', 'btn-danger', 'btn-sm')
+        btn.addEventListener('click', () => { DeleteItem(i) });
+
         sum += items[i].sum;
+
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
@@ -87,69 +101,42 @@ function RefreshTable(){
         tr.appendChild(td6);
         td6.appendChild(btn);
 
+
         itemsList.appendChild(tr);
-       
     }
     sumLbl.innerHTML = sum;
-    
 }
 
-function ClearForm(){
+function ClearForm() {
     nameField.value = '';
     priceField.value = 0;
     countField.value = 0;
 }
 
-function Save(){
-    localStorage.setItem('bevlista', JSON.stringify(items));
-    localStorage.setItem('product', JSON.stringify(productList));
+function Save() {
+    localStorage.setItem('bevLista', JSON.stringify(items));
+    localStorage.setItem('products', JSON.stringify(products));
 }
 
-function Load(){
-    if(localStorage.getItem('bevlista')){
-        items = JSON.parse(localStorage.getItem('bevlista'));
-    }
-}
-function Delete(index){
-    if(confirm("Biztosan törölni szeretnéd?")){
-        items.splice(index, 1)
-    }
-    RefreshTable();
-    Save();
-}
-
-function LoadProduct(){
-    productInList.innerHTML = "";
-    if(localStorage.getItem('product')){
-        productList = JSON.parse(localStorage.getItem('product'));
+function Load() {
+    if (localStorage.getItem("bevLista")) {
+        items = JSON.parse(localStorage.getItem("bevLista"));
     }
 
-    for (let i = 0; i < productList.length; i++) {
-        let option = document.createElement('option');
-        option.innerHTML = productList[i].name;
-        priceField.value = productList[i].price;
-
-        productInList.appendChild(option)
-    }
-    
-}
-
-function Valtozik(){
-    for (let i = 0; i < productList.length; i++) {
-        alert(productList[i].name + ' - ' + productList[i].price)
-        alert(productInList.value)
-        if(productInList.value == productList[i].name){
-            priceField.innerHTML = 1
-        }
-        
+    if (localStorage.getItem("products")) {
+        products = JSON.parse(localStorage.getItem("products"));
     }
 }
 
-//App indítása, törli a beviteli mezőt, betölti a tömböt, és kiírja a táblázatba.
-ClearForm();
-LoadProduct();
+function DeleteItem(idx) {
+    if (confirm("Biztosan törlöd a tételt?")) {
+        items.splice(idx, 1);
+        RefreshTable();
+        Save();
+    }
+
+}
+
 Load();
 RefreshTable();
-
-
-//768 Egységár és mennyiség egymás mellett, 992 mindhárom egymás mellett
+ClearForm();
